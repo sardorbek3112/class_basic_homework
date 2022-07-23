413e9e8e944be183e2d9b943d573cc6025e8786a
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-23 12:17:44 +0500

    create_one_attribute.py

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,5 +1,5 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
 class Person:
-    def __init__(name):
-        pass
+    def __init__(self,name):
+        self.name = name
